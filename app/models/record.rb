171e436725d01c7fe03f718e59069e0ccfc84161
Ramil Assanov <aramille@googlemail.com>
2022-06-02 12:34:52 +0200

    add records edit and show

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,5 +1,5 @@
 class Record < ApplicationRecord
   belongs_to :user
-  has_many :record_symptoms
+  has_many :record_symptoms, dependent: :destroy
   has_many :symptoms, through: :record_symptoms
 end
