b307cb5bb4882b01df7ea12c52da5b8b360b2f84
Ramil Assanov <aramille@googlemail.com>
2022-06-06 11:14:18 +0200

    add cloudinary to production

--- app/models/record.rb
+++ app/models/record.rb
@@ -2,4 +2,5 @@ class Record < ApplicationRecord
   belongs_to :user
   has_many :record_symptoms, dependent: :destroy
   has_many :symptoms, through: :record_symptoms
+  has_one_attached :photo
 end
